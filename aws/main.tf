40c721d89ed8f5fe31afda384c234db27fddf760
leonardomm10 <leom.mendonca10@gmail.com>
2022-12-29 15:44:51 -0300

    Terraform AWS organize code

--- aws/main.tf
+++ aws/main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
-}
-
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.centos7.id
   instance_type          = "t2.micro"
